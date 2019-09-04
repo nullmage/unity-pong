44e6b138c475af100e65aabe90ea133fa2c6a045
nullmage <jsky.johnson@gmail.com>
2019-09-04 14:46:54 -0600

    Changes things

--- Assets/BallControl.cs
+++ Assets/BallControl.cs
@@ -5,23 +5,20 @@ using UnityEngine;
 public class BallControl : MonoBehaviour {
 
 	private Rigidbody2D rb2d;
+    public GameObject Particle;
 
 	void GoBall() {
-		float rand = Random.Range (0, 2);
-		if (rand < 1) {
-			rb2d.AddForce (new Vector2 (20, -15));
-		} else {
-			rb2d.AddForce (new Vector2 (-20, -15));
-		}
+        rb2d.AddForce(40 * Random.insideUnitCircle);
 	}
 
-	// Use this for initialization
-	void Start () {
-		rb2d = GetComponent<Rigidbody2D> ();
-		Invoke ("GoBall", 2);
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("GoBall", 2);
+    }
 
-	void ResetBall() {
+    void ResetBall() {
 		rb2d.velocity = new Vector2 (0, 0);
 		transform.position = Vector2.zero;
 	}
@@ -32,12 +29,32 @@ public class BallControl : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
-		if (coll.collider.CompareTag ("Player")) {
-			Vector2 vel;
-			vel.x = rb2d.velocity.x;
-			vel.y = (rb2d.velocity.y / 2.0f) + (coll.collider.attachedRigidbody.velocity.y / 3.0f);
-			rb2d.velocity = vel;
-		}
+
+
+
+        if (coll.collider.CompareTag("Player"))
+        {
+            GameObject clones = Instantiate(Particle, transform);
+            clones.GetComponent<Rigidbody2D>().velocity = new Vector2(rb2d.velocity.x*2f, rb2d.velocity.y) + Random.insideUnitCircle;
+
+            GameObject clone2 = Instantiate(Particle, transform);
+            clone2.GetComponent<Rigidbody2D>().velocity = new Vector2(rb2d.velocity.x*3f, rb2d.velocity.y) + Random.insideUnitCircle;
+
+            GameObject clone3 = Instantiate(Particle, transform);
+            clone3.GetComponent<Rigidbody2D>().velocity = new Vector2(rb2d.velocity.x*1.4f, rb2d.velocity.y) + Random.insideUnitCircle;
+
+            GameObject clone4 = Instantiate(Particle, transform);
+            clone4.GetComponent<Rigidbody2D>().velocity = new Vector2(rb2d.velocity.x*2.4f, rb2d.velocity.y) + Random.insideUnitCircle;
+
+
+
+            Vector2 vel;
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y / 2.0f) + (coll.collider.attachedRigidbody.velocity.y / 3.0f) + new Vector3(0f, transform.position.y - coll.transform.position.y, 0f).y;
+            rb2d.velocity = vel;
+        }
+
+
 	}
 
 }
---
+++ Assets/Particle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    public float destroyTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, destroyTime);
+    }
+
+    // Update is called once per fram
+    void Update()
+    {
+        this.transform.localScale = .999f*this.transform.localScale ;
+    }
+}
